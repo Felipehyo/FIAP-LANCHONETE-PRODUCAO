bf08dc0c980b82f1c456a77df66eb26eb40d6e99
Felipe Pinheiro <felipehyo@gmail.com>
2024-12-03 19:26:54 -0300

    Adjusts

--- src/test/java/com/postech/ApplicationTest.java
+++ src/test/java/com/postech/ApplicationTest.java
@@ -2,12 +2,14 @@ package com.postech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ApplicationTest {
 
     @Test
     void contextLoads() {
-        Application.main(new String[] {});
+//        Application.main(new String[] {});
     }
 }
